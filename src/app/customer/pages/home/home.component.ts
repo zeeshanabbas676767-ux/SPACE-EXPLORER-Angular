a5e2c226f0d3c1458e4d4d6c31973772ddb4eefa
Zeeshan Abbas <zeeshanabbas676767@gmail.com>
2026-09-20 12:34:26 +0500

    NEW UPDATE 04

--- src/app/customer/pages/home/home.component.ts
+++ src/app/customer/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-home',
+  imports: [RouterLink],
 templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
 }
